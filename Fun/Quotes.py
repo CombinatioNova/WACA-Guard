1c42dd2e2c22ab3acc4becd7be5f0cbdf5cbc044
CombinatioNova <53846838+CombinatioNova@users.noreply.github.com>
2023-07-09 20:01:31 -0400

    More bugfix and feature add

--- Fun/Quotes.py
+++ Fun/Quotes.py
@@ -1,5 +1,6 @@
 import disnake
 from disnake.ext import commands
+from disnake import ApplicationCommandInteraction
 import sqlite3
 from datetime import datetime
 
@@ -8,7 +9,7 @@ class QuotesCog(commands.Cog):
         self.bot = bot
         self.db_connection = sqlite3.connect('reacted_messages.db')
         self.create_table()
-    
+
     def create_table(self):
         cursor = self.db_connection.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS reacted_messages
@@ -35,6 +36,11 @@ class QuotesCog(commands.Cog):
                 await quotes_channel.send(quoted_text)
                 await reaction.message.add_reaction("📸")
 
+    @commands.Cog.listener()
+    async def on_application_command_error(self, ctx, error):
+        if isinstance(error, commands.errors.CommandNotFound):
+            return
+
     def is_message_reacted(self, message_id):
         cursor = self.db_connection.cursor()
         cursor.execute("SELECT message_id FROM reacted_messages WHERE message_id = ?", (message_id,))
@@ -45,11 +51,36 @@ class QuotesCog(commands.Cog):
         cursor = self.db_connection.cursor()
         cursor.execute("INSERT INTO reacted_messages (message_id) VALUES (?)", (message_id,))
         self.db_connection.commit()
-        
+
     def remove_mentions(self, content, mentions):
         for mention in mentions:
             content = content.replace(mention.mention, mention.display_name)
         return content
+
+    @commands.message_command(name="Snapshot")
+    async def snapshot(self, inter, message):
+        
+        if message:
+            current_year = datetime.now().year
+            quotes_channel_name = "💬│quotes"  # Default channel name
+            quotes_channel = disnake.utils.get(message.guild.channels, name=quotes_channel_name)
+            if (
+                quotes_channel
+                and message.channel != quotes_channel
+                and message.author != self.bot.user
+                and not self.is_message_reacted(message.id)
+            ):
+                self.add_reacted_message(message.id)
+                print("Add!")
+                quoted_content = self.remove_mentions(message.content, message.mentions)
+                print("Removed mentions!")
+                quoted_text = f"\"{quoted_content}\" - {message.author.display_name.capitalize()}, {datetime.now().year}"
+                await quotes_channel.send(quoted_text)
+                print("Sent!")
+                await message.add_reaction("📸")
+                await inter.response.send_message("Done!", ephemeral=True)
+            else:
+                print("No...")
 
 def setup(bot):
     bot.add_cog(QuotesCog(bot))
